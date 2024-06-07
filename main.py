8a1004b141307e438f5126dfd582932c18f96909
Nick Salander <vankalif@gmail.com>
2024-06-07 14:45:17 +0300

    v 1.0.0

--- main.py
+++ main.py
@@ -9,28 +9,48 @@ import requests
 import ffmpeg
 import queue
 
+from requests.auth import HTTPDigestAuth
+
+# Globals
 SERVER_NAME = "TEST-SERVER"
 APP_TOKEN = "y0_AgAAAABl3sHIAAvpVwAAAAEGxjnpAABpGseraHRK3bgTIqzeVm3n_DDVVg"
-urls = [
+NUMBER_OF_THREADS = 5
+URLS = [
     {"resource": "http://admin:123456zxC@192.168.110.2:80/ISAPI/Streaming/channels/1/picture", "name": "Камера_1"},
     {"resource": "http://admin:123456zxC@192.168.110.3:80/ISAPI/Streaming/channels/1/picture", "name": "Камера_2"},
     {"resource": "http://admin:123456zxC@192.168.110.4:80/ISAPI/Streaming/channels/1/picture", "name": "Камера_3"},
     {"resource": "http://admin:123456zxC@192.168.110.5:80/ISAPI/Streaming/channels/1/picture", "name": "Камера_4"},
     {"resource": "http://admin:123456zxC@192.168.110.6:80/ISAPI/Streaming/channels/1/picture", "name": "Камера_5"}
 ]
-q = queue.Queue()
+Q = queue.Queue()
+CLIENT = yadisk.Client(token=APP_TOKEN)
+CLEAR_OFFSET = 72
+
+
+def init_folders():
+    if not CLIENT.exists(f"/{SERVER_NAME}"):
+        CLIENT.mkdir(f"/{SERVER_NAME}")
+
+    for resource in URLS:
+        if not CLIENT.exists(f"/{SERVER_NAME}/{resource["name"]}"):
+            CLIENT.mkdir(f"/{SERVER_NAME}/{resource["name"]}")
+
+
+def clear_folder(path: str, offset: int, perm=True):
+    dir_struct = CLIENT.listdir(path)
+    for file in dir_struct:
+        if (datetime.datetime.now(datetime.timezone.utc) - file.created) > datetime.timedelta(hours=offset):
+            CLIENT.remove(file.path, permanently=perm)
 
 
 def salt(size=6, chars=string.ascii_uppercase + string.digits):
     return ''.join(random.choice(chars) for _ in range(size))
 
 
-def send_to_cloud(source, destination, token):
-    client = yadisk.Client(token=token)
-
-    with client:
-        if client.check_token():
-            client.upload(source, destination)
+def send_to_cloud(source, destination):
+    with CLIENT:
+        if CLIENT.check_token():
+            CLIENT.upload(source, destination)
             os.remove(source)
 
 
@@ -38,8 +58,11 @@ def compress_image(input_image_path, output_image_path, quality_scale=2):
     ffmpeg.input(input_image_path).output(output_image_path, qscale=quality_scale).run()
 
 
-def load_shot(url) -> str:
-    bdata = requests.get(url).content
+def load_shot(source) -> str:
+    bdata = requests.get(source,
+                         auth=HTTPDigestAuth('admin', '123456zxC'),
+                         verify=False,
+                         stream=True).content
     path = os.path.expandvars("${TEMP}\\" + f"{str(uuid.uuid4())}.jpg")
 
     with open(path, 'wb') as file:
@@ -48,21 +71,40 @@ def load_shot(url) -> str:
     return path
 
 
-def process_image(info):
-    _input = load_shot(info.resource)
-    datetime_now = datetime.datetime.now()
-    filename = datetime_now.strftime("%d%m%y-%H-%M-%S-") + salt()
-    _output = os.path.expandvars("${TEMP}\\" + f"{filename}.jpg")
-    compress_image(_input, _output, 20)
-    destination = SERVER_NAME + "/" + info.name + "/" + filename
-    send_to_cloud(_output, destination, APP_TOKEN)
+def worker():
+    while True:
+        if Q.empty():
+            break
+
+        cam_info = Q.get(block=False)
+        url = cam_info["resource"]
+        channel_folder = cam_info["name"]
+        _input = load_shot(url)
+        datetime_now = datetime.datetime.now()
+        filename = datetime_now.strftime("%d%m%y-%H-%M-%S-") + salt()
+        _output = os.path.expandvars("${TEMP}\\" + f"{filename}.jpg")
+        compress_image(_input, _output, 20)
+        destination = "/" + SERVER_NAME + "/" + channel_folder + "/" + f"{filename}.jpg"
+        send_to_cloud(_output, destination)
+        os.remove(_input)
+        clear_folder("/" + SERVER_NAME + "/" + channel_folder, CLEAR_OFFSET)
+        Q.task_done()
 
 
 if __name__ == '__main__':
-    for url in urls:
-        q.put(url)
+    init_folders()
 
-    while not q.empty():
-        thread1 = threading.Thread(target=process_image, args=q.get())
-        thread1.start()
-        thread1.join()
+    for url in URLS:
+        Q.put(url)
+
+    threads = []
+    for _ in range(NUMBER_OF_THREADS):
+        thread = threading.Thread(target=worker)
+        thread.start()
+        threads.append(thread)
+
+    Q.join()
+
+    for thread in threads:
+        thread.join()
+
